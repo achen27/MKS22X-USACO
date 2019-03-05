64dc02898e762d1acd44b9475bfcfa1f8ef21e1f
Amanda Chen <achen01@stuy.edu>
2019-03-05 12:13:24 -0500

    started bronze method and creating new file and scanner

---
+++ USACO.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class USACO {
+
+  public static int bronze(String filename){
+    try {
+      File f = new File(filename);
+      Scanner s = new Scanner(f);
+    } catch (FileNotFoundException e){
+      System.out.println("File not found");
+    }
+
+  }
+
+  //public static int silver(String filename)
+
+}
