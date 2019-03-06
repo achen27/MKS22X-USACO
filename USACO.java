4b7908cadfdeac437318c1a4641cf15e3bfa973e
Amanda Chen <achen01@stuy.edu>
2019-03-05 20:32:42 -0500

    scanned pasture into 2D array

--- USACO.java
+++ USACO.java
@@ -5,20 +5,33 @@ public class USACO {
 
   public static int bronze(String filename){
     try {
-      int[][] pasture;
-      int row;
-      int col;
-      int elevation;
-      int instructions;
       int[][] list;
 
       File f = new File(filename);
       Scanner s = new Scanner(f);
+
+      //reads first line of file and places valeus into variables
+      int row;
+      int col;
+      int elevation;
+      int instructions;
+
       row = s.nextInt();
       col = s.nextInt();
       elevation = s.nextInt();
       instructions = s.nextInt();
-      System.out.println(row + " " + col + " " + elevation + " " + instructions);
+      //System.out.println(row + " " + col + " " + elevation + " " + instructions);
+
+      //reading pasture elevations onto a 2D array
+      int[][] pasture = new int[row][col];
+      for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+          pasture[i][j] = s.nextInt();
+        }
+      }
+
+      System.out.println(Arrays.deepToString(pasture));
+
     } catch (FileNotFoundException e){
       System.out.println("File not found");
     }
