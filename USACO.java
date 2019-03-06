ef73a12c5fd3b7872f3a6c187902ea708fc6f002
Amanda Chen <achen01@stuy.edu>
2019-03-05 20:28:19 -0500

    scanned first line into variables

--- USACO.java
+++ USACO.java
@@ -5,14 +5,30 @@ public class USACO {
 
   public static int bronze(String filename){
     try {
+      int[][] pasture;
+      int row;
+      int col;
+      int elevation;
+      int instructions;
+      int[][] list;
+
       File f = new File(filename);
       Scanner s = new Scanner(f);
+      row = s.nextInt();
+      col = s.nextInt();
+      elevation = s.nextInt();
+      instructions = s.nextInt();
+      System.out.println(row + " " + col + " " + elevation + " " + instructions);
     } catch (FileNotFoundException e){
       System.out.println("File not found");
     }
-
+    return 0;
   }
 
+  public static void main(String[] args){
+    bronze("makelake.1.in");
+
+  }
   //public static int silver(String filename)
 
 }
